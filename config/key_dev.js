65351ea9bcd605ac75b31dba09b3d85709983db5
Fabrice N'DRI <32721495+nyf005@users.noreply.github.com>
2021-11-01 04:49:26 +0000

    Delete key_dev.js

--- config/key_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://nyf.dev:SecretDev1@ds155352.mlab.com:55352/marine-dev",
-  cloudinaryName: "nyf005",
-  cloudinaryApiKey: "693376615368616",
-  cloudinaryApiSecret: "J05PBww9lrbGVPpNMze5PMp_2uA"
-};
